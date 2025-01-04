43e037b429d5934ef3bb2cc9e888bf9a321fca62
Sushants-Git <sushantsgml@gmail.com>
2025-01-04 23:11:30 +0530

    chore : remove tiptap

--- src/components/editor/body.tsx
+++ src/components/editor/body.tsx
@@ -1,37 +1,8 @@
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import History from "@tiptap/extension-history";
-import ListItem from "@tiptap/extension-list-item";
-import OrderedList from "@tiptap/extension-ordered-list";
-import BulletList from "@tiptap/extension-bullet-list";
-import {
-    Editor,
-    EditorContent,
-    useEditor,
-    useEditorState,
-} from "@tiptap/react";
-
 import React from "react";
 
 import type { BodyRef } from "./types/body.types";
 
 import "./styles/body.css";
-
-const extensions = [
-    Document,
-    Paragraph,
-    Text,
-    History,
-    OrderedList,
-    BulletList,
-    ListItem,
-];
-
-const editorOptions = {
-    extensions,
-    content: "",
-};
 
 enum Key {
     EMPTY = "",
@@ -40,7 +11,6 @@ enum Key {
 }
 
 const Body = React.forwardRef<BodyRef>((_, bodyRef) => {
-    const editor = useEditor(editorOptions);
     const decoratorKeyPress = React.useRef(Key.EMPTY);
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -49,51 +19,11 @@ const Body = React.forwardRef<BodyRef>((_, bodyRef) => {
         }
     };
 
-    useEditorState({
-        editor,
-        selector: ({ editor }) => {
-            if (!editor) return;
-
-            handleCodeStyling(editor, decoratorKeyPress);
-        },
-    });
-
     React.useImperativeHandle(bodyRef, () => ({
-        focusBody: () => editor?.commands.focus("start"),
+        focusBody: () => {},
     }));
 
-    return (
-        <>
-            <EditorContent
-                editor={editor}
-                className="*:outline-none editor-body leading-relaxed"
-                onKeyDown={handleKeyDown}
-            />
-        </>
-    );
+    return <></>;
 });
-
-const handleCodeStyling = (
-    editor: Editor,
-    decoratorKeyPress: React.MutableRefObject<Key>
-) => {
-    if (decoratorKeyPress.current !== Key.BACK_TICK) return;
-
-    decoratorKeyPress.current = Key.EMPTY;
-
-    // Hello| -> H = 1, textCursor = 6  (according to Tiptap)
-    // Hello| -> H = 0, textCursor = 5  (their indexes)
-
-    // We are looking for -> " `" (i.e SPACE followed by BACK_TICK)
-
-    const textCursorIndex = editor.state.selection.to - 1;
-    const spaceBeforeApostrophe =
-        editor.getText().at(textCursorIndex - 2) === Key.SPACE;
-
-    if (spaceBeforeApostrophe) {
-        editor.commands.insertContent(Key.BACK_TICK);
-        editor.commands.focus(textCursorIndex + 1);
-    }
-};
 
 export default Body;
--- src/components/editor/title.tsx
+++ src/components/editor/title.tsx
@@ -1,9 +1,3 @@
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import History from "@tiptap/extension-history";
-import { EditorContent, JSONContent, useEditor } from "@tiptap/react";
-
 import React from "react";
 
 import type { TitleProps, TitleRef } from "./types/title.types";
@@ -12,46 +6,12 @@ const title = "Untitled";
 
 const Title = React.forwardRef<TitleRef, TitleProps>(
     ({ handleEnter }, titleRef) => {
-        const editor = useEditor({
-            extensions: [Document, Paragraph, Text, History],
-            content: title,
-        });
-
         React.useImperativeHandle(titleRef, () => ({
-            toSinglePara() {
-                const editiorContent = editor?.getJSON().content;
-
-                if (editiorContent && editiorContent.length > 1) {
-                    const arr: string[] = [];
-                    iterateObject(editiorContent, arr);
-                    editor.commands.setContent(`<p>${arr.join("")}</p>`);
-                }
-            },
+            toSinglePara() { },
         }));
 
-        return (
-            <>
-                <EditorContent
-                    editor={editor}
-                    className="*:outline-none"
-                    onKeyDown={handleEnter}
-                />
-            </>
-        );
+        return <></>;
     }
 );
-
-function iterateObject(obj: JSONContent, contentString: string[]) {
-    for (const key in obj) {
-        const isObject = typeof obj[key] === "object" && obj[key] !== null;
-        const keyIsText = key === "text";
-
-        if (isObject) {
-            iterateObject(obj[key], contentString);
-        } else if (keyIsText) {
-            contentString.push(obj["text"] as string);
-        }
-    }
-}
 
 export default Title;
